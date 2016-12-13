437b9a5f7dfa65812a14e18b7b80198aa6577808
rhys5690 <rhys5690@gmail.com>
2016-12-14 10:24:43 +1100

    added seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,11 +40,141 @@ u4 = User.create( {
   :isadmin => true
 } )
 
+u5 = User.create( {
+  :first_name => "Rhys",
+  :last_name => "Diab",
+  :email => "rhys5690@gmail.com",
+  :username => "rhys5690",
+  :isadmin => true
+} )
+
+u6 = User.create( {
+  :first_name => "John",
+  :last_name => "Stupid",
+  :email => "jstupid@gmail.com",
+  :username => "jstupid",
+  :isadmin => false
+} )
+
+u7 = User.create( {
+  :first_name => "Blue",
+  :last_name => "Whale",
+  :email => "bluewhale@gmail.com",
+  :username => "bluewhale",
+  :isadmin => false
+} )
+
+u8 = User.create( {
+  :first_name => "big",
+  :last_name => "man",
+  :email => "bigman@gmail.com",
+  :username => "bigman",
+  :isadmin => false
+} )
+
+u9 = User.create( {
+  :first_name => "small",
+  :last_name => "dog",
+  :email => "smalldoggmail.com",
+  :username => "smalldog",
+  :isadmin => false
+} )
+
+u10 = User.create( {
+  :first_name => "big",
+  :last_name => "dog",
+  :email => "bigdog@gmail.com",
+  :username => "jstupid",
+  :isadmin => false
+} )
+
+u11 = User.create( {
+  :first_name => "mr",
+  :last_name => "joe",
+  :email => "mrjoe@gmail.com",
+  :username => "mrjoe",
+  :isadmin => false
+} )
+
+u12 = User.create( {
+  :first_name => "tell",
+  :last_name => "thedog",
+  :email => "tellthedog@gmail.com",
+  :username => "tellthedog",
+  :isadmin => false
+} )
+
+u13 = User.create( {
+  :first_name => "ilikecheese",
+  :last_name => "yes",
+  :email => "ilikecheese@gmail.com",
+  :username => "ilikecheese,"
+  :isadmin => false
+} )
+
+u14 = User.create( {
+  :first_name => "stop",
+  :last_name => "stopit",
+  :email => "stopit@gmail.com",
+  :username => "stopit",
+  :isadmin => false
+} )
+
+u15 = User.create( {
+  :first_name => "hi",
+  :last_name => "there",
+  :email => "hithere@gmail.com",
+  :username => "hithere",
+  :isadmin => false
+} )
+
+u16 = User.create( {
+  :first_name => "go",
+  :last_name => "away",
+  :email => "goaway@gmail.com",
+  :username => "jstupid",
+  :isadmin => false
+} )
+
+u17 = User.create( {
+  :first_name => "never",
+  :last_name => "gone",
+  :email => "nevergone@gmail.com",
+  :username => "nevergone",
+  :isadmin => false
+} )
+
+u18 = User.create( {
+  :first_name => "ilike",
+  :last_name => "dogs",
+  :email => "ilikedogs@gmail.com",
+  :username => "ilikedogs",
+  :isadmin => false
+} )
+
+u19 = User.create( {
+  :first_name => "ihatecats",
+  :last_name => "yesido",
+  :email => "ihatecats@gmail.com",
+  :username => "ihatecats",
+  :isadmin => false
+} )
+
+u20 = User.create( {
+  :first_name => "mr",
+  :last_name => "t",
+  :email => "mrt@gmail.com",
+  :username => "mrttoday",
+  :isadmin => false
+} )
+
+
+
 ### Airplane
 a1 = Airplane.create( {
   :name => "737-800",
-  :rows => 30,
-  :columns => 6
+  :rows => 9,
+  :columns => 5
 } )
 
 a2 = Airplane.create( {
@@ -86,31 +216,159 @@ f4 = Flight.create( {
   :airplane_id => a2.id
 } )
 
-### Reservation
-r1 = Reservation.create( {
-  :seat_row => "4",
-  :seat_column => "B",
-  :flight_id => f3.id,
-  :user_id => u3.id
+f5 = Flight.create( {
+  :flight_num => "I240",
+  :origin => "Sydney",
+  :destination => "Brisbane",
+  :flight_datetime => DateTime.new( 2016, 12, 31 ),
+  :airplane_id => a1.id
 } )
 
-r2 = Reservation.create( {
-  :seat_row => "8",
+f6 = Flight.create( {
+  :flight_num => "I240",
+  :origin => "Brisbane",
+  :destination => "Sydney",
+  :flight_datetime => DateTime.new( 2016, 12, 31 ),
+  :airplane_id => a2.id
+} )
+
+### Reservation
+r1 = Reservation.create( {
+  :seat_row => "1",
   :seat_column => "A",
   :flight_id => f1.id,
   :user_id => u1.id
 } )
 
+r2 = Reservation.create( {
+  :seat_row => "1",
+  :seat_column => "B",
+  :flight_id => f1.id,
+  :user_id => u2.id
+} )
+
 r3 = Reservation.create( {
-  :seat_row => "9",
+  :seat_row => "1",
   :seat_column => "C",
-  :flight_id => f2.id,
-  :user_id => u4.id
+  :flight_id => f1.id,
+  :user_id => u3.id
 } )
 
 r4 = Reservation.create( {
+  :seat_row => "1",
+  :seat_column => "D",
+  :flight_id => f1.id,
+  :user_id => u4.id
+} )
+
+r5 = Reservation.create( {
+  :seat_row => "1",
+  :seat_column => "E",
+  :flight_id => f1.id,
+  :user_id => u20.id
+} )
+
+r6 = Reservation.create( {
+  :seat_row => "2",
+  :seat_column => "A",
+  :flight_id => f1.id,
+  :user_id => u5.id
+} )
+
+r7 = Reservation.create( {
+  :seat_row => "2",
+  :seat_column => "B",
+  :flight_id => f1.id,
+  :user_id => u6.id
+} )
+
+r8 = Reservation.create( {
+  :seat_row => "2",
+  :seat_column => "C",
+  :flight_id => f1.id,
+  :user_id => u7.id
+} )
+
+r9 = Reservation.create( {
+  :seat_row => "2",
+  :seat_column => "D",
+  :flight_id => f1.id,
+  :user_id => u8.id
+} )
+
+r10 = Reservation.create( {
   :seat_row => "2",
   :seat_column => "E",
-  :flight_id => f4.id,
-  :user_id => u2.id
+  :flight_id => f1.id,
+  :user_id => u9.id
+} )
+
+r11 = Reservation.create( {
+  :seat_row => "3",
+  :seat_column => "A",
+  :flight_id => f1.id,
+  :user_id => u10.id
+} )
+
+r12 = Reservation.create( {
+  :seat_row => "3",
+  :seat_column => "B",
+  :flight_id => f1.id,
+  :user_id => u11.id
+} )
+
+r13 = Reservation.create( {
+  :seat_row => "3",
+  :seat_column => "C",
+  :flight_id => f1.id,
+  :user_id => u12.id
+} )
+
+r14 = Reservation.create( {
+  :seat_row => "3",
+  :seat_column => "D",
+  :flight_id => f1.id,
+  :user_id => u13.id
+} )
+
+r15 = Reservation.create( {
+  :seat_row => "3",
+  :seat_column => "E",
+  :flight_id => f1.id,
+  :user_id => u14.id
+} )
+
+r16 = Reservation.create( {
+  :seat_row => "4",
+  :seat_column => "A",
+  :flight_id => f1.id,
+  :user_id => u15.id
+} )
+
+r17 = Reservation.create( {
+  :seat_row => "4",
+  :seat_column => "B",
+  :flight_id => f1.id,
+  :user_id => u16.id
+} )
+
+r18 = Reservation.create( {
+  :seat_row => "4",
+  :seat_column => "C",
+  :flight_id => f1.id,
+  :user_id => u17.id
+} )
+
+r19 = Reservation.create( {
+  :seat_row => "4",
+  :seat_column => "E",
+  :flight_id => f1.id,
+  :user_id => u18.id
+} )
+
+r20 = Reservation.create( {
+  :seat_row => "6",
+  :seat_column => "C",
+  :flight_id => f1.id,
+  :user_id => u19.id
 } )
