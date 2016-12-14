29c70554bb904f0d822f552840d8d57725daf919
ewelina-borucka <ewelina.borucka@gmail.com>
2016-12-14 13:52:55 +1100

    created JavaScript file structure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation )
     end
 end
